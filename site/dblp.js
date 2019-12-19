8b13b25c0d9bd78b62624f9fd51c44083a4fab2c
hnshhslsh <hnshhslsh@163.com>
2019-12-19 18:31:32 +0800

    Update DBLP

--- site/dblp.js
+++ site/dblp.js
@@ -3,41 +3,61 @@ const dblp = {};
 dblp.rankingSpanProvider = [];
 
 dblp.start = function(){
-    let interval = setInterval(function(){
-        let url = window.location.href;
-        let title = $('head > title').text();
-        if(url.startsWith("https://dblp.uni-trier.de/search?") || title.indexOf("Search for")!=-1){
+    let url = window.location.href;
+    let title = $('head > title').text();
+    if(url.startsWith("https://dblp.uni-trier.de/search?") || title.indexOf("Search for")!=-1){
+        let interval = setInterval(function(){
             let message = $('#completesearch-publs > div > p.waiting');
-            
+    
             if(message.css('display') == "none"){
                 $(window).bind('popstate', function(){
                     dblp.addRankings();
                 });
                 dblp.addRankings();
             }
+        }, 1000);
+    } else if(url.startsWith("https://dblp.uni-trier.de/pers/")) {
+        dblp.addRankings();
+    } else if(url.startsWith("https://dblp.uni-trier.de/db/conf/") || url.startsWith("https://dblp.uni-trier.de/db/journals/")) {
+        // "https://dblp.uni-trier.de/db/".length == 29
+        let start = url.indexOf("/", 29) + 1;
+        let abbr = url.substring(start, url.indexOf("/",start)).toUpperCase();
+        if(url.endsWith(".html") && !url.endsWith("index.html")){
+            dblp.addRanking("#breadcrumbs > ul > li > span:nth-child(3) > a > span", abbr);
+        } else {
+            dblp.addRanking("h1", abbr);
         }
-    }, 1000);
+    } 
 }
 
 dblp.addRankings = function(){
-    let results = $("article > a > span:nth-child(1) > span:nth-child(1)");//获取期刊或会议名称
+    let results = $("cite > a > span:nth-child(1) > span:nth-child(1)");//获取期刊或会议名称
     dblp.resultsCount = results.length;
-
+    
     results.each(function(index){
         let result = $(this);
         let source = result.text().trim();
-        if(source.length != 0){
-            let names = dblp.parseNames(source);
+        if(source.length != 0 && !result.next().hasClass('ccf-ranking')){
             for(let getRankingSpan of dblp.rankingSpanProvider){
-                if($(this).next().hasClass('ccf-ranking')){
-                    result.after('');
-                }else{
-                    result.after(getRankingSpan(names));
-                }
+                let names = dblp.parseNames(source);
+                result.after(getRankingSpan(names));
             }
         }
-        
     });
+}
+
+dblp.addRanking = function(selector, abbr){
+    let result = $(selector);
+    let source = result.text().trim();
+    if(abbr != undefined && source.indexOf('(') == -1){
+        source = source + "(" + abbr + ")";
+    }
+    if(source.length != 0){
+        for(let getRankingSpan of dblp.rankingSpanProvider){
+            let names = dblp.parseNames(source);
+            result.after(getRankingSpan(names));
+        }
+    }
 }
 
 dblp.parseNames = function(source){
