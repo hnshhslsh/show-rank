5be0c05afc7b1f6beed3d8e141ae507878772fd2
hnshhslsh <hnshhslsh@163.com>
2020-11-08 00:07:35 +0800

    Update DBLP

--- site/dblp.js
+++ site/dblp.js
@@ -16,12 +16,10 @@ dblp.start = function(){
                 dblp.addRankings();
             }
         }, 1000);
-    } else if(url.startsWith("https://dblp.uni-trier.de/pers/") || url.startsWith("https://dblp.org/pers/")) {
+    } else if(url.startsWith("https://dblp.uni-trier.de/pid/") || url.startsWith("https://dblp.org/pid/")) {
         dblp.addRankings();
-    } else if(url.startsWith("https://dblp.uni-trier.de/db/conf/") || url.startsWith("https://dblp.uni-trier.de/db/journals/") || url.startsWith("https//dblp.org/db/conf/") || url.startsWith("https://dblp.org/db/journals/")) {
-        // "https://dblp.uni-trier.de/db/".length == 29
-        let start = url.indexOf("/", 29) + 1;
-        let abbr = url.substring(start, url.indexOf("/",start)).toUpperCase();
+    } else if(url.startsWith("https://dblp.uni-trier.de/db/conf/") || url.startsWith("https://dblp.uni-trier.de/db/journals/") || url.startsWith("https://dblp.org/db/conf/") || url.startsWith("https://dblp.org/db/journals/")) {
+        let abbr = dblp.getAbbrFromUrl(url)
         if(url.endsWith(".html") && !url.endsWith("index.html")){
             dblp.addRanking("#breadcrumbs > ul > li > span:nth-child(3) > a > span", abbr);
         } else {
@@ -30,13 +28,33 @@ dblp.start = function(){
     } 
 }
 
+dblp.getAbbrFromUrl = function (url) {
+    let start;
+    if (url.startsWith("https://dblp.uni-trier.de/db/")) {
+        start = url.indexOf("/", 29) + 1; // "https://dblp.uni-trier.de/db/".length == 29
+    } else if (url.startsWith("https://dblp.org/db/")) {
+        start = url.indexOf("/", 20) + 1; // "https://dblp.org/db/".length == 20
+    } else {
+        return undefined;
+    }
+    return url.substring(start, url.indexOf("/", start)).toUpperCase();
+}
+
 dblp.addRankings = function(){
     let results = $("cite > a > span:nth-child(1) > span:nth-child(1)");//获取期刊或会议名称
     dblp.resultsCount = results.length;
     
     results.each(function(index){
         let result = $(this);
-        let source = result.text().trim();
+        let source = result.text().trim().replace(/\(\d+\)/, "");
+
+        // get abbr name from href, thanks @HJhao (https://github.com/HJhao/show-rank/commit/1e36249d10e72ea33007535a4b976f3be70a13d0)
+        let url = result.parent().parent().attr("href");
+        let abbr = dblp.getAbbrFromUrl(url);
+        if (abbr != undefined && source.indexOf('(') == -1) {
+            source = source + "(" + abbr + ")";
+        }
+
         if(source.length != 0 && !result.next().hasClass('ccf-ranking')){
             for(let getRankingSpan of dblp.rankingSpanProvider){
                 let names = dblp.parseNames(source);
